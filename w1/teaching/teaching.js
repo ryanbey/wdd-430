2cacfc9761d9a4c6b676a251f3c7baab39dea357
Ryan Bey <bey15002@byui.edu>
2022-04-23 21:35:27 -0600

    teaching week 1

---
+++ w1/teaching/teaching.js
@@ -0,0 +1,5 @@
+let a = 2;
+let b = 4;
+
+let answer = a + b;
+console.log("Your answer is: " + answer);
---
+++ w1/teaching/teaching.ts
@@ -0,0 +1,5 @@
+let d: string = 'two';
+let c: string = 'two';
+
+let answer2: string = c + d;
+console.log("Your answer is: " + answer2);
