966f5398b9045363047c9bc0b5d60ddc9ef92a8f
ryanbey <beycommaryan@gmail.com>
2022-04-26 09:27:39 -0600

    started cms assignment

--- w2/cms-project/src/app/app.component.ts
+++ w2/cms-project/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'cms-root',
+  // QUIZ Q: template or tempalteURL are required. This line links to HTML file for this component
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
