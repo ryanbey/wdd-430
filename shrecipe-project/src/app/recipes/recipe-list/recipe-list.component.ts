304795b81316883cfdd031f79663cf3fabee6758
Ryan Bey <bey15002@byui.edu>
2022-05-03 16:02:32 -0600

    clicking recipe items now displays recipe detail

--- shrecipe-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ shrecipe-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,20 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
-import { Recipe } from '../recipe.model'
+import { Recipe } from '../recipe.model';
 
 @Component({
-   selector: 'app-recipe-list',
-   templateUrl: './recipe-list.component.html',
-   styleUrls: ['./recipe-list.component.scss']
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.scss'],
 })
 export class RecipeListComponent implements OnInit {
-   recipes: Recipe[] = [
-      new Recipe('Test Recipe', 'Just a test.', 'https://images.pexels.com/photos/65175/pexels-photo-65175.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1.jpg')
-   ];
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
-   constructor() { }
+  recipes: Recipe[] = [
+    new Recipe(
+      'Yummy Steak',
+      'Get in my mouth.',
+      'https://images.pexels.com/photos/65175/pexels-photo-65175.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1.jpg'
+    ),
+    new Recipe(
+      'Is that bacon?',
+      'Yes.',
+      'https://images.pexels.com/photos/1927377/pexels-photo-1927377.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1.jpg'
+    ),
+  ];
 
-   ngOnInit(): void {
-   }
+  constructor() {}
 
+  ngOnInit(): void {}
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
