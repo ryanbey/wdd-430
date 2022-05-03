43a9353d3f38f8a8ef590fd33fbcc698b446406d
Ryan Bey <bey15002@byui.edu>
2022-05-03 16:36:21 -0600

    added ability to add to shopping list

--- shrecipe-project/src/app/shopping-list/shopping-list.component.ts
+++ shrecipe-project/src/app/shopping-list/shopping-list.component.ts
@@ -5,17 +5,19 @@ import { Ingredient } from '../shared/ingredient.model';
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.scss']
+  styleUrls: ['./shopping-list.component.scss'],
 })
 export class ShoppingListComponent implements OnInit {
   ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
-    new Ingredient('Tomatoes', 10)
+    new Ingredient('Tomatoes', 10),
   ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onIngredientAdded(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
   }
-
 }
