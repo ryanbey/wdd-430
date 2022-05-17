c86f975633d677570d7927eef1c724616d02cc7c
Ryan Bey <bey15002@byui.edu>
2022-05-17 15:23:27 -0600

    finished add to shopping list button

--- shrecipe-project/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ shrecipe-project/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipesService } from '../recipes.service';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -9,7 +10,11 @@ import { Recipe } from '../recipe.model';
 export class RecipeDetailComponent implements OnInit {
   @Input() recipe: Recipe;
 
-  constructor() {}
+  constructor(private recipesService: RecipesService) {}
 
   ngOnInit(): void {}
+
+  onAddToShoppingList() {
+    this.recipesService.addIngredientsToShoppingList(this.recipe.ingredients);
+  }
 }
--- shrecipe-project/src/app/recipes/recipes.service.ts
+++ shrecipe-project/src/app/recipes/recipes.service.ts
@@ -1,4 +1,6 @@
 import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 import { Recipe } from './recipe.model';
 
 @Injectable({
@@ -7,22 +9,35 @@ import { Recipe } from './recipe.model';
 export class RecipesService {
   recipeSelected = new EventEmitter<Recipe>();
 
-  constructor() {}
+  constructor(private shoppingListService: ShoppingListService) {}
 
   private recipes: Recipe[] = [
     new Recipe(
       'Yummy Steak',
       'Get in my mouth.',
-      'https://images.pexels.com/photos/65175/pexels-photo-65175.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1.jpg'
+      'https://images.pexels.com/photos/65175/pexels-photo-65175.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1.jpg',
+      [
+        new Ingredient('8oz steak', 1),
+        new Ingredient('Green leaf thing', 1),
+        new Ingredient('Red pepper', 3),
+      ]
     ),
     new Recipe(
       'Is that bacon?',
       'Yes.',
-      'https://images.pexels.com/photos/1927377/pexels-photo-1927377.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1.jpg'
+      'https://images.pexels.com/photos/1927377/pexels-photo-1927377.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1.jpg',
+      [
+        new Ingredient('Pack of bacon', 1),
+        new Ingredient('Green stuff', 1)
+      ]
     ),
   ];
 
   getRecipes() {
-    return this.recipes.slice();  // Return a copy of the local array
+    return this.recipes.slice(); // Return a copy of the local array
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredients);
   }
 }
