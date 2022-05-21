67f251b9a658e4008b62952bc43264980ac70c03
Ryan Bey <bey15002@byui.edu>
2022-05-21 16:50:10 -0600

    implemented contact and document services

--- cms-project/src/app/documents/documents.component.ts
+++ cms-project/src/app/documents/documents.component.ts
@@ -1,17 +1,22 @@
 import { Component, OnInit, Output } from '@angular/core';
+import { DocumentService } from './document.service';
 import { Document } from './documents.model';
 
 @Component({
   selector: 'cms-documents',
   templateUrl: './documents.component.html',
-  styleUrls: ['./documents.component.scss']
+  styleUrls: ['./documents.component.scss'],
 })
 export class DocumentsComponent implements OnInit {
   selectedDocument: Document;
 
-  constructor() { }
+  constructor(private documentService: DocumentService) {}
 
   ngOnInit(): void {
+    this.documentService.documentSelectedEvent.subscribe(
+      (document: Document) => {
+        this.selectedDocument = document;
+      }
+    );
   }
-
 }
