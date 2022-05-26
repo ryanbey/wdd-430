68f80a801b4e7b3f8484f1a0b1e0974b158172fb
ryanbey <beycommaryan@gmail.com>
2022-05-26 08:06:24 -0600

    fixed route parameters error

--- shrecipe-project/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ shrecipe-project/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -16,10 +16,10 @@ export class RecipeDetailComponent implements OnInit {
   constructor(private recipesService: RecipesService, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
-    this.route.params.subscribe(params: Params) => {
+    this.route.params.subscribe((params: Params) => {
       this.id = +params['id'];
       this.recipe = this.recipesService.getRecipe(this.id);
-    };
+    });
   }
 
   onAddToShoppingList() {
