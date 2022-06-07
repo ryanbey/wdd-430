a5e69c289b57e60882be6033a5b69469b44cb836
Ryan Bey <bey15002@byui.edu>
2022-06-07 17:26:27 -0600

    clear button working

--- shrecipe-project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ shrecipe-project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -37,7 +37,7 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     this.subscription.unsubscribe();
   }
 
-  onAddItem(form: NgForm) {
+  onSubmit(form: NgForm) {
     const value = form.value;
     const newIngredient = new Ingredient(value.name, value.amount);
     if (this.editMode) {
@@ -45,5 +45,12 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     } else {
       this.shoppingListService.addIngredient(newIngredient);
     }
+    this.editMode = false;
+    form.reset();
+  }
+
+  onClear() {
+    this.shoppingListForm.reset();
+    this.editMode = false;
   }
 }
