15262a772223962110b00797a6d7fef93a055a1c
ryanbey <beycommaryan@gmail.com>
2022-06-09 20:21:54 -0600

    contact edit form started

--- cms-project/src/app/contacts/contact-edit/contact-edit.component.ts
+++ cms-project/src/app/contacts/contact-edit/contact-edit.component.ts
@@ -7,11 +7,16 @@ import { Contact } from '../contact.model';
   styleUrls: ['./contact-edit.component.scss']
 })
 export class ContactEditComponent implements OnInit {
+  contact: Contact;
   groupContacts: Contact[];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(form) {
+    
   }
 
   onCancel() {
