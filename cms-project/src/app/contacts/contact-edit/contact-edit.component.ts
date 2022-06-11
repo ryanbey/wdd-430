1a7b4e28931bbea2ee1910dae6411f692839ec68
ryanbey <beycommaryan@gmail.com>
2022-06-10 18:04:51 -0600

    implemented drag and drop

--- cms-project/src/app/contacts/contact-edit/contact-edit.component.ts
+++ cms-project/src/app/contacts/contact-edit/contact-edit.component.ts
@@ -12,7 +12,7 @@ import { ContactService } from '../contact.service';
 export class ContactEditComponent implements OnInit {
   originalContact: Contact;
   contact: Contact;
-  groupContacts: Contact[];
+  groupContacts: Contact[] = [];
   editMode = false;
   id: string;
 
@@ -23,35 +23,36 @@ export class ContactEditComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    this.route.params.subscribe(
-      (params: Params) => {
-        const id = params['id'];
-        if (id === undefined || id === null) {
-          this.editMode = false;
-          return;
-        }
+    this.route.params.subscribe((params: Params) => {
+      const id = params['id'];
+      if (id === undefined || id === null) {
+        this.editMode = false;
+        return;
+      }
 
-        this.originalContact = this.contactService.getContact(id);
+      this.originalContact = this.contactService.getContact(id);
 
-        if (this.originalContact === undefined || this.originalContact === null) {
-          return;
-        }
-        this.editMode = true;
-        this.contact = JSON.parse(JSON.stringify(this.originalContact));
-        if (this.contact.group) {
-          this.groupContacts = [...this.contact.group];
-        }
+      if (this.originalContact === undefined || this.originalContact === null) {
+        return;
+      }
+      this.editMode = true;
+      this.contact = JSON.parse(JSON.stringify(this.originalContact));
+      if (this.contact.group) {
+        this.groupContacts = [...this.contact.group];
+      }
     });
   }
 
   onSubmit(form: NgForm) {
     const value = form.value;
-    const newContact = new Contact(this.contactService.maxContactId.toString(),
-                                     value.name,
-                                     value.email,
-                                     value.phone,
-                                     value.imageUrl,
-                                     null);
+    const newContact = new Contact(
+      this.contactService.maxContactId.toString(),
+      value.name,
+      value.email,
+      value.phone,
+      value.imageUrl,
+      null
+    );
     if (this.editMode) {
       this.contactService.updateContact(this.originalContact, newContact);
     } else {
@@ -62,5 +63,36 @@ export class ContactEditComponent implements OnInit {
 
   onCancel() {
     this.router.navigate(['/contacts']);
+  }
+
+  addToGroup($event: any) {
+    const selectedContact: Contact = $event.dragData;
+    const invalidGroupContact = this.isInvalidContact(selectedContact);
+    if (invalidGroupContact) {
+      return;
+    }
+    this.groupContacts.push(selectedContact);
+  }
+
+  isInvalidContact(newContact: Contact) {
+    if (!newContact) {
+      return true;
+    }
+    if (this.contact && newContact.id === this.contact.id) {
+      return true;
+    }
+    for (let i = 0; i < this.groupContacts.length; i++) {
+      if (newContact.id === this.groupContacts[i].id) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  onRemoveItem(index: number) {
+    if (index < 0 || index >= this.groupContacts.length) {
+      return;
+    }
+    this.groupContacts.splice(index, 1);
   }
 }
