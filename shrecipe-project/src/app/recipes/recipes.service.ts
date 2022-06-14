904381ac278693ba3f0bea3a58e4ca4e0014a78e
Ryan Bey <bey15002@byui.edu>
2022-06-14 14:54:36 -0600

    finished data storage service and added resolver

--- shrecipe-project/src/app/recipes/recipes.service.ts
+++ shrecipe-project/src/app/recipes/recipes.service.ts
@@ -9,30 +9,8 @@ import { Recipe } from './recipe.model';
 })
 export class RecipesService {
   constructor(private shoppingListService: ShoppingListService) {}
-
+  private recipes: Recipe[] = [];
   recipesChanged = new Subject<Recipe[]>();
-
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Yummy Steak',
-      'Get in my mouth.',
-      'https://images.pexels.com/photos/65175/pexels-photo-65175.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1.jpg',
-      [
-        new Ingredient('8oz steak', 1),
-        new Ingredient('Green leaf thing', 1),
-        new Ingredient('Red pepper', 3),
-      ]
-    ),
-    new Recipe(
-      'Is that bacon?',
-      'Yes.',
-      'https://images.pexels.com/photos/1927377/pexels-photo-1927377.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1.jpg',
-      [
-        new Ingredient('Pack of bacon', 1),
-        new Ingredient('Green stuff', 1)
-      ]
-    ),
-  ];
 
   setRecipes(recipes: Recipe[]) {
     this.recipes = recipes;
--- shrecipe-project/src/app/shared/data-storage.service.ts
+++ shrecipe-project/src/app/shared/data-storage.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { RecipesService } from '../recipes/recipes.service';
 import { Recipe } from '../recipes/recipe.model';
+import { map, tap } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
@@ -23,12 +24,23 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    this.http
+    return this.http
       .get<Recipe[]>(
         'https://angular-recipe-book-9c8ef-default-rtdb.firebaseio.com/recipes.json'
       )
-      .subscribe((recipes) => {
-        this.recipeService.setRecipes(recipes);
-      });
+      .pipe(
+        map((recipes) => {
+          return recipes.map((recipe) => {
+            // Make sure new recipes without ingredients still get an empty array for ingredients property
+            return {
+              ...recipe,
+              ingredients: recipe.ingredients ? recipe.ingredients : [],
+            };
+          });
+        }),
+        tap((recipes) => {
+          this.recipeService.setRecipes(recipes);
+        })
+      );
   }
 }
