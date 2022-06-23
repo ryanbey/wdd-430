9bdcb6a164ac89a595c8a1ff3686d3daf1f098c1
ryanbey <beycommaryan@gmail.com>
2022-06-23 08:38:02 -0600

    fixing path for mapping non-defined routes

--- cms-project/server.js
+++ cms-project/server.js
@@ -53,7 +53,7 @@ app.use('/documents', documentRoutes);
 
 // Tell express to map all other non-defined routes back to the index page
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist/cms/index.html"));
+  res.sendFile(path.join(__dirname, "dist/cms-project/index.html"));
 });
 
 // Define the port address and tell express to use this port
