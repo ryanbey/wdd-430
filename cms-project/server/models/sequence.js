74d3118887d01b037e6e110eb17fee764bc6a3e5
ryanbey <beycommaryan@gmail.com>
2022-06-30 08:17:09 -0600

    udpated sequence model, deleted example model

--- cms-project/server/models/sequence.js
+++ cms-project/server/models/sequence.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 
 const sequenceSchema = mongoose.Schema({
-  id: { type: String, required: true } // not sure about this one
+  maxDocumentId: { type: Number, required: true },
+  maxContactId: { type: Number, required: true },
+  maxMessageId: { type: Number, required: true }
 });
 
 module.exports = mongoose.model("Sequence", sequenceSchema);
